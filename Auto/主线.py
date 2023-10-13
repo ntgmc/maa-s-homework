4ac8f25b0eeb2a58b21342f81b2055af18910120
2586582719 <2586582719@qq.com>
2023-10-14 06:18:08 +0800

    主线13章&悖论模拟&模组任务更新&纷争演绎进入备份

--- Auto/主线.py
+++ Auto/主线.py
@@ -7,7 +7,7 @@ source_folder_base = r"D:\GITHOME\maa\主线"
 destination_folder = r"D:\GITHOME\maa\主线\压缩包"
 
 # 遍历需要压缩的文件夹并进行压缩
-for i in range(13):
+for i in range(14):
     # 构造源文件夹和目标压缩包文件路径
     source_folder_path = os.path.join(source_folder_base, "主线-第{:02d}章".format(i))
     destination_zip_path = os.path.join(destination_folder, "主线-第{:02d}章.zip".format(i))
