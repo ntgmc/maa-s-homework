72847ca1a30c7b210538aa936f133f92bb7a2d70
ntgmc <2586582719@qq.com>
2024-05-12 09:23:59 +0800

    适配sub关卡，增加往期剿灭搜索

--- Auto/Full-Auto/主线一键下载.py
+++ Auto/Full-Auto/主线一键下载.py
@@ -4,13 +4,12 @@ import re
 from datetime import datetime
 import os
 
-# 设置阈值
+# 设置阈值(好评率和浏览量)(不满足条件则降低阈值，但最低不低于50% 0)
 download_score_threshold = 80  # 好评率阈值
 download_view_threshold = 1000  # 浏览量阈值
 # 设置stage_name
 tough = [10, 11, 12, 13, 14]
 main = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-sub = [2, 3, 4, 5, 6, 7, 9]  # TODO: 增加sub适配
 # 设置最大关卡数
 max_level = {
     0: 11,
@@ -41,6 +40,17 @@ max_hard_level = {
     13: 4,
     14: 4
 }
+max_sub_level = {
+    2: 12,
+    3: 7,
+    4: 10,
+    5: 9,
+    6: 4,
+    7: 2,
+    9: 2
+}
+# 设置空列表
+no_result = []
 
 
 def get_current_date():
@@ -70,6 +80,19 @@ def build_dict(data, key: str):  # key为生成的字典的键
     return _dict
 
 
+def build_sub_dict(data):  # key为章节整数
+    _dict = {}
+    for member in data:
+        if 'obt/main/level_sub_' not in member['level_id']:  # 只处理sub关卡
+            continue
+        _key = get_sub_stage_info(member['stage_id'])
+        if _key in _dict:
+            _dict[_key].append(member)
+        else:
+            _dict[_key] = [member]
+    return _dict
+
+
 def get_level_data():
     url = 'https://prts.maa.plus/arknights/level'
     response = requests.get(url)
@@ -89,11 +112,11 @@ def calculate_percent(item):
         return round(like / total * 100, 2)
 
 
-def get_level_name(stage_id):
+def get_level_name(stage_id):  # 通过stage_id获取关卡名称
     return stage_dict.get(stage_id, [{}])[0].get('cat_three', '')
 
 
-def extract_info(text):  # TODO: 增加sub适配
+def get_stage_info(text):
     match = re.search(r"(\d+)-", text)
     if match:
         return int(match.group(1))
@@ -101,18 +124,36 @@ def extract_info(text):  # TODO: 增加sub适配
         return None
 
 
-def generate_filename_mode3(data):
-    stage_name = get_level_name(data.get('stage_name', ''))
-    _stage = extract_info(stage_name)
+def get_sub_stage_info(text):
+    match = re.search(r"sub_(\d+)-", text)
+    if match:
+        return int(match.group(1))
+    else:
+        return None
+
+
+def replace_special_char(text):
+    return text.replace('/', '').replace('\\', '')
+
+
+def generate_filename(name, data, mode):
+    stage_name = get_level_name(name)
+    _stage = get_stage_info(stage_name)
     opers = data.get('opers', [])
     groups = data.get('groups', [])
     names_parts = ['+'.join(oper.get('name', '') for oper in opers),
                    '+'.join(group.get('name', '') for group in groups)]
     names = '+'.join(part for part in names_parts if part)  # 只连接非空的部分
-    return f'./download/主线/第{_stage}章/{stage_name}_{names.replace("/", "")}.json'
+    if mode == 1 or mode == 3:
+        return f'./download/主线/第{_stage}章/{stage_name}_{replace_special_char(names)}.json'
+    elif mode == 2:
+        return f'./download/往期剿灭/{stage_name}_{replace_special_char(names)}.json'
+    else:
+        return f'./download/{stage_name}_{replace_special_char(names)}.json'
 
 
-def search(keyword):
+def search(keyword, mode=1):
+    global no_result
     url = f"https://prts.maa.plus/copilot/query?page=1&limit=15&levelKeyword={keyword}&desc=true&orderBy=views"
     _headers = {
         "Origin": "https://prts.plus",
@@ -133,7 +174,7 @@ def search(keyword):
                     view = item.get('views', 0)
                     if percent >= score_threshold and view >= view_threshold:
                         content = json.loads(item['content'])
-                        file_path = generate_filename_mode3(content)
+                        file_path = generate_filename(keyword, content, mode)
                         content['doc']['details'] = f"统计日期：{date}\n好评率：{percent}%  浏览量：{view}\n来源：{item['uploader']}  ID：{item['id']}\n" + content['doc']['details']
                         print(f"{file_path} {percent}% {view} 成功下载")
                         write_to_file(file_path, content)
@@ -143,6 +184,10 @@ def search(keyword):
                         score_threshold -= 5
                     else:
                         view_threshold -= 200
+                    if view_threshold < 0:
+                        print(f"{keyword} 无符合50% 0的数据 不再重试")
+                        no_result.append(keyword)
+                        break
                     print(f"{keyword} 无符合条件的数据，降低阈值为{score_threshold}% {view_threshold}重试")
         else:
             print(f"{keyword} 无数据")
@@ -165,19 +210,38 @@ def hard_stage_search(_stage):
         search(f"hard_{pad_zero(_stage)}-{pad_zero(level)}")
 
 
-def bat_search():
+def sub_stage_search(_stage):
+    for member in sub_dict.get(_stage, []):
+        search(f"{member['stage_id']}", 3)
+
+
+def camp_stage_search():
+    for level in range(1, 4):
+        search(f"camp_{pad_zero(level)}", 2)
+    for level in range(1, len(cat_one_dict.get('剿灭作战', [])) - 2):
+        search(f"camp_r_{pad_zero(level)}", 2)
+
+
+def main_bat_search():
     for _stage in tough:
         tough_stage_search(_stage)
     for _stage in main:
         main_stage_search(_stage)
     for _stage in max_hard_level:
         hard_stage_search(_stage)
+    for _stage in max_sub_level:
+        sub_stage_search(_stage)
 
 
-if not os.path.exists(f'./download/主线'):
-    os.makedirs(f'./download/主线')
+if not os.path.exists(f'./download/往期剿灭'):
+    os.makedirs(f'./download/往期剿灭')
 for stage in max_level:
     if not os.path.exists(f'./download/主线/第{stage}章'):
         os.makedirs(f'./download/主线/第{stage}章')
-stage_dict = build_dict(get_level_data(), 'stage_id')
-bat_search()
+level_data = get_level_data()
+stage_dict = build_dict(level_data, 'stage_id')
+sub_dict = build_sub_dict(level_data)
+cat_one_dict = build_dict(level_data, 'cat_one')
+main_bat_search()
+camp_stage_search()
+print('No_result: ', no_result)
