9f554e3852f7b310d242ccd24d0bb7e4aa0f97f9
ntgmc <2586582719@qq.com>
2024-06-10 17:17:46 +0800

    perf: 作者最终表

--- Auto/统计作业.py
+++ Auto/统计作业.py
@@ -1,10 +1,13 @@
 # import requests
 import json
+import csv
 
 amount_dict = {}
 views_dict = {}
 likes_dict = {}
 dislikes_dict = {}
+max_views_dict = {}
+max_likes_dict = {}
 
 # url = "https://prts.maa.plus/copilot/query?page=1&limit=99999&levelKeyword=&desc=true&orderBy=views"
 # _headers = {
@@ -26,6 +29,10 @@ for member in data:
     likes_dict[member["uploader"]] = likes_dict.get(member["uploader"], 0) + member["like"]
     dislikes_dict["total"] = dislikes_dict.get("total", 0) + member["dislike"]
     dislikes_dict[member["uploader"]] = dislikes_dict.get(member["uploader"], 0) + member["dislike"]
+    max_views_dict["total"] = max(max_views_dict.get("total", 0), member["views"])
+    max_views_dict[member["uploader"]] = max(max_views_dict.get(member["uploader"], 0), member["views"])
+    max_likes_dict["total"] = max(max_likes_dict.get("total", 0), member["like"])
+    max_likes_dict[member["uploader"]] = max(max_likes_dict.get(member["uploader"], 0), member["like"])
 amount_sorted_dict = {k: v for k, v in sorted(amount_dict.items(), key=lambda item: item[1], reverse=True)}
 views_sorted_dict = {k: v for k, v in sorted(views_dict.items(), key=lambda item: item[1], reverse=True)}
 likes_sorted_dict = {k: v for k, v in sorted(likes_dict.items(), key=lambda item: item[1], reverse=True)}
@@ -54,7 +61,26 @@ for user in amount_sorted_dict.keys():
         '每作业浏览量': round(views_dict[user] / amount_dict[user], 2) if amount_dict[user] != 0 else 0,
         '每作业好评数': round(likes_dict[user] / amount_dict[user], 2) if amount_dict[user] != 0 else 0,
         '每作业好评率': round(likes_dict[user] / (likes_dict[user] + dislikes_dict[user]) * 100, 2) if likes_dict[user] + dislikes_dict[user] != 0 else 0,
+        '最大单作业浏览量': max_views_dict[user],
+        '最大单作业好评数': max_likes_dict[user],
     }
 # 保存整合数据
 with open('user_profiles.json', 'w', encoding='utf-8') as file:
     json.dump(user_profiles, file, ensure_ascii=False, indent=4)
+
+# Get all keys from the user_profiles dictionary, excluding 'average'
+all_keys = set().union(*(d.keys() for k, d in user_profiles.items() if k != 'average'))
+
+# Prepare CSV file's headers (column names)
+headers = ['key'] + list(all_keys)
+
+# Write into CSV file
+with open('user_profiles.csv', 'w', encoding='utf-8-sig', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
+    for key, value in user_profiles.items():
+        if key == 'average':  # Skip the 'average' key
+            continue
+        row = {'key': key}
+        row.update(value)
+        writer.writerow(row)
