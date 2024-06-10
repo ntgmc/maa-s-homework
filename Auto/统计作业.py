cb57c7e0fe4ac0535e0e9c7d0183bfd9235da747
ntgmc <2586582719@qq.com>
2024-06-10 16:18:30 +0800

    feat: 作者对应作业数,浏览量数,好评数输出

--- Auto/统计作业.py
+++ Auto/统计作业.py
@@ -1,7 +1,10 @@
 import requests
 import json
 
-dict = {}
+amount_dict = {}
+views_dict = {}
+likes_dict = {}
+# point_dict = {}
 
 url = "https://prts.maa.plus/copilot/query?page=1&limit=99999&levelKeyword=&desc=true&orderBy=views"
 _headers = {
@@ -13,8 +16,19 @@ response = requests.get(url, headers=_headers)
 print("Success")
 data = response.json()["data"]["data"]
 for member in data:
-    dict[member["uploader"]] = dict.get(member["uploader"], 0) + 1
-    dict["total"] = dict.get("total", 0) + 1
-sorted_dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1], reverse=True)}
-with open("output.json", 'w', encoding='utf-8') as file:
-    json.dump(sorted_dict, file, ensure_ascii=False, indent=4)
+    amount_dict[member["uploader"]] = amount_dict.get(member["uploader"], 0) + 1
+    amount_dict["total"] = amount_dict.get("total", 0) + 1
+    views_dict[member["uploader"]] = views_dict.get(member["uploader"], 0) + member["views"]
+    views_dict["total"] = views_dict.get("total", 0) + member["views"]
+    likes_dict[member["uploader"]] = likes_dict.get(member["uploader"], 0) + member["like"]
+    likes_dict["total"] = likes_dict.get("total", 0) + member["like"]
+    # point_dict[member["uploader"]] = point_dict.get(member["uploader"], 0) + member["point"]
+amount_sorted_dict = {k: v for k, v in sorted(amount_dict.items(), key=lambda item: item[1], reverse=True)}
+views_sorted_dict = {k: v for k, v in sorted(views_dict.items(), key=lambda item: item[1], reverse=True)}
+likes_sorted_dict = {k: v for k, v in sorted(likes_dict.items(), key=lambda item: item[1], reverse=True)}
+with open("amount.json", 'w', encoding='utf-8') as file:
+    json.dump(amount_sorted_dict, file, ensure_ascii=False, indent=4)
+with open("views.json", 'w', encoding='utf-8') as file:
+    json.dump(views_sorted_dict, file, ensure_ascii=False, indent=4)
+with open("likes.json", 'w', encoding='utf-8') as file:
+    json.dump(likes_sorted_dict, file, ensure_ascii=False, indent=4)
