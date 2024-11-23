6ab9ea91ca8af6f4fa400602511012f5cf508858
ntgmc <2586582719@qq.com>
2024-11-23 16:41:13 +0800

    feat: 支持拆分悖论模组cache

--- Auto/Full-Auto/tool/convert_cache.py
+++ Auto/Full-Auto/tool/convert_cache.py
@@ -10,6 +10,7 @@ old_cache_file_path = 'Auto/Full-Auto/cache/cache.json'
 old_cache_file_path2 = 'Auto/Full-Auto/cache/activity_cache.json'
 # 新缓存文件路径
 new_cache_file_path = 'Auto/Full-Auto/cache/new_cache.json'
+new_cache_file_path1 = 'Auto/Full-Auto/cache/new_main_cache.json'
 new_cache_file_path2 = 'Auto/Full-Auto/cache/new_activity_cache.json'
 
 
@@ -44,7 +45,7 @@ def get_cat_three_info(cat_three_dict, cat_three, key):  # 通过cat_three获取
     return cat_three_dict.get(cat_three, [{}])[0].get(key, cat_three)
 
 
-def convert_old_cache_to_new(old_path, new_path):
+def convert_old_cache_to_new(old_path, new_path, new_path2):
     # 加载旧缓存数据
     with open(old_path, 'r', encoding='utf-8') as file:
         old_cache_data = json.load(file)
@@ -54,24 +55,30 @@ def convert_old_cache_to_new(old_path, new_path):
         "悖论": {},
         "模组": {}
     }
+    new_cache_data2 = {}
 
     # 转换旧缓存数据到新缓存数据结构
     for key, value in old_cache_data.items():
         parts = key.split('-')
-        if len(parts) == 3:
+        if "悖论" in parts or "模组" in parts:
             _id, name, category = parts
-            if category == '悖论':
-                if name not in new_cache_data["悖论"]:
-                    new_cache_data["悖论"][name] = {}
-                new_cache_data["悖论"][name][_id] = value
-            elif category == '模组':
-                if name not in new_cache_data["模组"]:
-                    new_cache_data["模组"][name] = {}
-                new_cache_data["模组"][name][_id] = value
+            if name not in new_cache_data[category]:
+                new_cache_data[category][name] = {}
+            new_cache_data[category][name][_id] = value
+        else:
+            _id, rest = key.split('-', 1)
+            chapter = get_cat_three_info(cat_three_all_dict, rest, "cat_two")
+            if chapter not in new_cache_data2:
+                new_cache_data2[chapter] = {}
+            if rest not in new_cache_data2[chapter]:
+                new_cache_data2[chapter][rest] = {}
+            new_cache_data2[chapter][rest][_id] = value
 
     # 保存新缓存数据
     with open(new_path, 'w', encoding='utf-8') as file:
         json.dump(new_cache_data, file, ensure_ascii=False, indent=4)
+    with open(new_path2, 'w', encoding='utf-8') as file:
+        json.dump(new_cache_data2, file, ensure_ascii=False, indent=4)
 
 
 def convert_old_activity_cache_to_new(old_path, new_path):
@@ -100,5 +107,5 @@ def convert_old_activity_cache_to_new(old_path, new_path):
 level_data = get_level_data()
 cat_three_all_dict = build_dict(level_data, "cat_three")
 # 调用函数进行转换
-convert_old_cache_to_new(old_cache_file_path, new_cache_file_path)
+convert_old_cache_to_new(old_cache_file_path, new_cache_file_path, new_cache_file_path1)
 convert_old_activity_cache_to_new(old_cache_file_path2, new_cache_file_path2)
